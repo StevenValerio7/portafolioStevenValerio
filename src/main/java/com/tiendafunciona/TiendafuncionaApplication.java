ca4314eef80f30d06981ee4cfabd2532b77d39bc
StevenValerio7 <stevenvalerio7@gmail.com>
2025-11-02 18:07:14 -0600

    primer committ

---
+++ src/main/java/com/tiendafunciona/TiendafuncionaApplication.java
@@ -0,0 +1,13 @@
+package com.tiendafunciona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendafuncionaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendafuncionaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendafunciona/TiendafuncionaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendafunciona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendafuncionaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
